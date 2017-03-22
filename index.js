26f05ace313af7ffc120c572de8cbf5aa9851800
Omid Hezaveh <omid.hezaveh@futurice.com>
2017-03-22 19:16:24 +0200

    add route

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({ host: '0.0.0.0', port: process.env.PORT || 8000 })
+
+server.route({
+    method: 'POST',
+    path:'/',
+    handler: function (request, reply) {
+
+        return reply('hello world');
+    }
+});
+
 server.start((err) => {
   if (err) throw err;
 
