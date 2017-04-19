c575870f0c8523e7e2e0086c41bfae3bc2556b59
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-04-19 14:03:19 +0300

    Add GET /test route

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
-server.connection({ host: '0.0.0.0', port: process.env.PORT || 8000 })
+server.connection({ host: '0.0.0.0', port: process.env.PORT || 8000 });
+server.route({
+  method: 'GET',
+  path: '/test',
+  handler: (request, reply) => reply('Test'),
+});
 server.start((err) => {
   if (err) throw err;
 
